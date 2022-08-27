f93d53e9b610498067f8729644e061e7bb112054
Sascha Weyers <sw@mailguru.io>
2022-08-27 19:16:55 +0200

    code refactored and tests added

--- src/main/kotlin/io/mailguru/api/core/model/ApiError.kt
+++ src/main/kotlin/io/mailguru/api/core/model/ApiError.kt
@@ -2,32 +2,85 @@ package io.mailguru.api.core.model
 
 import io.mailguru.api.core.exception.SourceException
 
+/**
+ * Representation of a JSON:API error; encapsulated into the `errors` property of an [ApiErrorResponse].
+ * <p>
+ * See <a href="https://jsonapi.org/format/#error-objects">JSON:API Error objects</a>.
+ * @see <a href="https://jsonapi.org/format/#error-objects">JSON:API Error objects</a>
+ *
+ * @constructor Creates an instance with the given properties.
+ * @param id A unique identifier for this particular occurrence of the problem.
+ * @param about A link that leads to further details about this particular occurrence of the problem.
+ * @param status The HTTP status code applicable to this problem, expressed as a string value.
+ * @param code An application-specific error code, expressed as a string value.
+ * @param title A short, human-readable summary of the problem that **SHOULD NOT** change from occurrence to occurrence
+ *              of the problem, except for purposes of localization.
+ * @param detail A human-readable explanation specific to this occurrence of the problem. Like `title`, this field’s
+ *               value can be localized.
+ * @param source An object containing references to the source of the error, see [ApiErrorSource].
+ * @param meta A meta object containing non-standard meta-information about the error.
+ */
 class ApiError(
-    val code: Int,
-    val title: String,
-    val detail: String,
-    parameter: String? = null,
-    propertyPath: String? = null
+    val id: String? = null,
+    val about: String? = null,
+    val status: String? = null,
+    val code: String? = null,
+    val title: String? = null,
+    val detail: String? = null,
+    val source: ApiErrorSource? = null,
+    val meta: Any? = null,
 ) {
 
+    /**
+     * Convenience constructor.
+     */
+    @Deprecated("Use primary constructor instead.")
+    constructor(
+        code: Int,
+        title: String,
+        detail: String,
+        parameter: String? = null,
+        propertyPath: String? = null
+    ) : this(
+        code = code.toString(),
+        title = title,
+        detail = detail,
+        source = ApiErrorSource(
+            parameter = parameter,
+            propertyPath = propertyPath
+        )
+    )
+
+    /**
+     * Convenience constructor.
+     */
+    @Deprecated("Use primary constructor instead.")
     constructor(
         status: Int,
         reasonPhrase: String,
         sourceException: SourceException,
     ) : this(
-        status,
-        reasonPhrase,
-        sourceException.message,
-        sourceException.parameter,
-        sourceException.propertyPath,
+        status = status.toString(),
+        code = status.toString(),
+        title = reasonPhrase,
+        detail = sourceException.message,
+        source = ApiErrorSource(
+            parameter = sourceException.parameter,
+            propertyPath = sourceException.propertyPath
+        )
     )
 
-    val source: ApiErrorSource = ApiErrorSource(parameter = parameter, propertyPath = propertyPath)
-
-    class ApiErrorSource(
-        val parameter: String? = null,
-        propertyPath: String? = null,
-    ) {
-        val pointer: String? = propertyPath?.let { "/${it}" }
-    }
+    /**
+     * Convenience constructor.
+     */
+    @Deprecated("Use primary constructor instead.")
+    constructor(
+        code: Int,
+        title: String,
+        detail: String,
+    ) : this(
+        code = code.toString(),
+        title = title,
+        detail = detail,
+    )
 }
--- src/main/kotlin/io/mailguru/api/core/model/ApiErrorResponse.kt
+++ src/main/kotlin/io/mailguru/api/core/model/ApiErrorResponse.kt
@@ -1,9 +1,27 @@
 package io.mailguru.api.core.model
 
+/**
+ * Representation of a JSON:API errors (top-level) document; encapsulates one of more
+ * [ApiError]s (as an array under the `errors` key) into a single document that is going to be returned to the client.
+ * <p>
+ * See <a href="https://jsonapi.org/format/#error-objects">JSON:API Error objects</a>.
+ * @see <a href="https://jsonapi.org/format/#error-objects">JSON:API Error objects</a>
+ *
+ * @constructor Creates an instance with the given collection of errors.
+ * @param errors A collection hat represents the errors occurred on processing an API request.
+ *               In terms of JSON:API spec, this parameter is going to be serialized as a JSON array in the response.
+ */
 class ApiErrorResponse(
-    val errors: Collection<ApiError> = listOf()
+    val errors: Collection<ApiError> = emptySet()
 ) {
-    constructor(error: ApiError) : this(listOf(error))
 
+    /**
+     * Creates an instance with a single-item collection of the given error.
+     */
+    constructor(error: ApiError) : this(setOf(error))
+
+    /**
+     * Helper method for mapping the contents of errors into JSON.
+     */
     fun toAttributeMap(): MutableMap<String, Any> = mutableMapOf(ApiErrorResponse::errors.name to errors)
 }
